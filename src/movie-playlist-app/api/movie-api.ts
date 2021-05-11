89f0ebb5cad3ace25da466deebbd0ad31f314e0b
Cody Burt <cody.burt.21@gmail.com>
2021-05-11 15:52:10 -0700

    add https

--- src/movie-playlist-app/api/movie-api.ts
+++ src/movie-playlist-app/api/movie-api.ts
@@ -5,7 +5,7 @@ export async function getSearchResults(
   page: number
 ): Promise<Movie[]> {
   const apiResponse = await fetch(
-    `http://www.omdbapi.com/?apikey=85f6b945&s=${query}&type=movie&page=${page}`
+    `https://www.omdbapi.com/?apikey=85f6b945&s=${query}&type=movie&page=${page}`
   )
   const responseBody = await apiResponse.json()
   return responseBody["Search"] ?? []
